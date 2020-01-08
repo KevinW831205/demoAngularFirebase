08a924160b16aaca32df573a313e3d77035cd2ee
KevinW <bird831205@hotmail.com>
2020-01-08 09:07:48 -0500

    config firebase in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBkStqCyeNj-2RbAtckAAD8wvG2VPMGbO8",
+    authDomain: "demoangularfirebase-9a5b9.firebaseapp.com",
+    databaseURL: "https://demoangularfirebase-9a5b9.firebaseio.com",
+    projectId: "demoangularfirebase-9a5b9",
+    storageBucket: "demoangularfirebase-9a5b9.appspot.com",
+    messagingSenderId: "399806563162",
+    appId: "1:399806563162:web:dd1edfb4556419ad076963",
+    measurementId: "G-26NZQ4BJ53"
+  }
 };
 
 /*
